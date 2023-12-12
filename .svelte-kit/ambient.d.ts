c9f30fa78aecd9352c2757f4bdc7706a8cf2d1a7
Tjips <thomas@scheepers.com>
2023-12-12 14:22:59 +0100

    oke?

--- .svelte-kit/ambient.d.ts
+++ .svelte-kit/ambient.d.ts
@@ -37,7 +37,9 @@ declare module '$env/static/private' {
 	export const ComSpec: string;
 	export const DriverData: string;
 	export const EDITOR: string;
-	export const EFC_8680: string;
+	export const EFC_8812: string;
+	export const FPS_BROWSER_APP_PROFILE_STRING: string;
+	export const FPS_BROWSER_USER_PROFILE_STRING: string;
 	export const GIT_ASKPASS: string;
 	export const HOME: string;
 	export const HOMEDRIVE: string;
@@ -148,7 +150,9 @@ declare module '$env/dynamic/private' {
 		ComSpec: string;
 		DriverData: string;
 		EDITOR: string;
-		EFC_8680: string;
+		EFC_8812: string;
+		FPS_BROWSER_APP_PROFILE_STRING: string;
+		FPS_BROWSER_USER_PROFILE_STRING: string;
 		GIT_ASKPASS: string;
 		HOME: string;
 		HOMEDRIVE: string;
